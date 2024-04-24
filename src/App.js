b4eedb374558a0c64e044a107d2553fcfe85867a
showole sheriff <showolesheriff7@gmail.com>
2024-04-23 23:49:56 -0700

    deleted

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
 import Main from './views/main';
-import serviceWorker from  './service-worker'
-const checkPermission=()=>{
-  if(!('serviceWorker' in navigator)){
-    throw new Error('no support for service worker');
-  }
-}
-const registerSW=async ()=>{
-  const registration= await navigator.serviceWorker.register('service-worker.js')
-  return registration;
-}
-checkPermission();
-registerSW().then((e)=>[
-  console.log(' sw created')
-]).catch((e)=>{
-  console.log(e)
-})
+// const checkPermission=()=>{
+//   if(!('serviceWorker' in navigator)){
+//     throw new Error('no support for service worker');
+//   }
+// }
+// const registerSW=async ()=>{
+//   const registration= await navigator.serviceWorker.register('service-worker.js')
+//   return registration;
+// }
+// checkPermission();
+// registerSW().then((e)=>[
+//   console.log(' sw created')
+// ]).catch((e)=>{
+//   console.log(e)
+// })
 function App() {
   return (
     <div className="App">
